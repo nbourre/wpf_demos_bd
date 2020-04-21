1523fd3cfd24e50fda58ae7716576f7567693e0c
Nicolas Bourré <nbourrereg@hotmail.com>
2020-04-21 17:00:28 -0400

    Démarrage de l'affichage des employés direct

---
+++ App.Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.Models
+{
+    public class Employee
+    {
+        public int EmployeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string HomePhone { get; set; }
+
+    }
+}
---
+++ demo_02_connection_commands/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows;
+
+namespace demo_02_connection_commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataTable dataTable;
+        bool dataLoaded = false;
+        List<string> fullNames = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData_Click(object sender, RoutedEventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("Select * from Employees", conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                        {
+                            dataTable = new DataTable();
+                            dataTable.Load(dataReader);
+
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("An error occured!");
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+
+            foreach (DataColumn column  in dataTable.Columns)
+            {
+                Debug.WriteLine(column.ColumnName);
+            }
+
+            foreach (DataRow row in dataTable.Rows) {
+                string fullName = $"{row["LastName"]}, {row["FirstName"]}";
+
+                fullNames.Add(fullName);
+            }
+
+            lvEmployee.ItemsSource = fullNames;
+            
+        }
+    }
+}

0f9ffbff44992215faa97e765b1002811fe2a850
Nicolas Bourré <nbourrereg@hotmail.com>
2020-04-24 14:16:17 -0400

    RelayCommand

--- wpf_demo_phonebook/ViewModels/MainViewModel.cs
+++ wpf_demo_phonebook/ViewModels/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Windows;
+using wpf_demo_phonebook.ViewModels.Commands;
 
 namespace wpf_demo_phonebook.ViewModels
 {
@@ -17,9 +17,50 @@ namespace wpf_demo_phonebook.ViewModels
             }
         }
 
+        private string criteria;
+
+        public string Criteria
+        {
+            get { return criteria; }
+            set { 
+                criteria = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand SearchContactCommand { get; set; }
+
         public MainViewModel()
         {
+            SearchContactCommand = new RelayCommand(SearchContact);
             SelectedContact = PhoneBookBusiness.GetContactByID(1);
+        }
+
+        private void SearchContact(object parameter)
+        {
+            string input = parameter as string;
+            int output;
+            string searchMethod;
+            if (!Int32.TryParse(input, out output))
+            {
+                searchMethod = "name";
+            } else
+            {
+                searchMethod = "id";
+            }
+
+            switch (searchMethod)
+            {
+                case "id":
+                    SelectedContact = PhoneBookBusiness.GetContactByID(output);
+                    break;
+                case "name":
+                    SelectedContact = PhoneBookBusiness.GetContactByName(input);
+                    break;
+                default:
+                    MessageBox.Show("Unkonwn search method");
+                    break;
+            }
         }
     }
 }

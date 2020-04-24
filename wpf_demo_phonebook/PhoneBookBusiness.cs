69b29c6a711ed4fb0e9c20f2a6f02dce3b416e03
Nicolas Bourré <nbourrereg@hotmail.com>
2020-04-24 13:05:29 -0400

    PhonebookBusiness added

--- wpf_demo_phonebook/PhoneBookBusiness.cs
+++ wpf_demo_phonebook/PhoneBookBusiness.cs
@@ -5,13 +5,13 @@ using System.Text;
 
 namespace wpf_demo_phonebook
 {
-    class PhoneBookBusiness
+    static class PhoneBookBusiness
     {
         private static PhonebookDAO dao = new PhonebookDAO();
 
         public static ContactModel GetContactByName(string _name)
         {
-            ContactModel cm = new ContactModel();
+            ContactModel cm = null;
 
             DataTable dt = new DataTable();
 
@@ -21,14 +21,42 @@ namespace wpf_demo_phonebook
             {
                 foreach (DataRow row in dt.Rows)
                 {
-                    cm.ContactID = Convert.ToInt32(row["ContactID"]);
-                    cm.FirstName = row["FirstName"].ToString();
-                    cm.LastName = row["LastName"].ToString();
-                    cm.Email = row["Email"].ToString();
-                    cm.Phone = row["Phone"].ToString();
-                    cm.Mobile = row["Mobile"].ToString();
+                    cm = RowToContactModel(row);
                 }
             }
+
+            return cm;
+        }
+
+        public static ContactModel GetContactByID(int _id)
+        {
+            ContactModel cm = null;
+
+            DataTable dt = new DataTable();
+
+            dt = dao.SearchByID(_id);
+
+            if (dt != null)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    cm = RowToContactModel(row);
+                }
+            }
+
+            return cm;
+        }
+
+        private static ContactModel RowToContactModel(DataRow row)
+        {
+            ContactModel cm = new ContactModel();
+
+            cm.ContactID = Convert.ToInt32(row["ContactID"]);
+            cm.FirstName = row["FirstName"].ToString();
+            cm.LastName = row["LastName"].ToString();
+            cm.Email = row["Email"].ToString();
+            cm.Phone = row["Phone"].ToString();
+            cm.Mobile = row["Mobile"].ToString();
 
             return cm;
         }
